5fdcd6aaa395d9c8abd3e7502a00444a4b999455
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-06-30 00:10:57 +0600

    Start writting js code for game initialization

---
+++ game.js
@@ -0,0 +1,29 @@
+var gameBoard = document.getElementById('game-board');
+var row0 = gameBoard.querySelectorAll("[data-row='0']");
+var row1 = gameBoard.querySelectorAll("[data-row='1']");
+var row2 = gameBoard.querySelectorAll("[data-row='2']");
+var row3 = gameBoard.querySelectorAll("[data-row='3']");
+var row4 = gameBoard.querySelectorAll("[data-row='4']");
+var row5 = gameBoard.querySelectorAll("[data-row='5']");
+var row6 = gameBoard.querySelectorAll("[data-row='6']");
+var row7 = gameBoard.querySelectorAll("[data-row='7']");
+
+/**
+ * ============================
+ * Game initialization section
+ * ============================
+ * */ 
+function initializeGame(params) {
+  // set checker with appropriate data
+
+}
+
+// top part
+function initializeBlackChecker(params) {
+  
+}
+
+// lower part
+function initializeRedChecker(params) {
+  
+}
