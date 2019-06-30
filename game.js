57c8fc27f24e618de5197a1a341d0a6c175adee4
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-06-30 23:55:42 +0600

    Add jquery for easy dom manipulation
    -Create checker games board

--- game.js
+++ game.js
@@ -1,82 +1,121 @@
-var gameBoard = document.getElementById('game-board');
-var row0 = gameBoard.querySelectorAll("[data-row='0']");
-var row1 = gameBoard.querySelectorAll("[data-row='1']");
-var row2 = gameBoard.querySelectorAll("[data-row='2']");
-var row3 = gameBoard.querySelectorAll("[data-row='3']");
-var row4 = gameBoard.querySelectorAll("[data-row='4']");
-var row5 = gameBoard.querySelectorAll("[data-row='5']");
-var row6 = gameBoard.querySelectorAll("[data-row='6']");
-var row7 = gameBoard.querySelectorAll("[data-row='7']");
-console.log('---file is loading------');
-alert('this is alert');
-/**
- * ============================
- * Game initialization section
- * ============================
- * */ 
-function initializeGame() {
-  console.log('-------- game is initializing---------');
-  console.log('-----------row7', row7);
-  // set checker with appropriate data
-  var cell = row7.querySelector("[data-col=7]");
-  createBlackChecker($cell);
-}
 
-// top part
-function initializeBlackChecker() {
-  
-}
+$(function() {
+	console.log("this si asdfasdf");
+	var $gameBoard = $('#game-board');
+	var $blackChecker = $('<div class="checker black-checker js-black-checker"></div>');
+	var $redChecker = $('<div class="checker red-checker js-red-checker"></div>');
+	// initial board setting, 1 means red checker, 2 means black checker
+	var gameBoardArray = [
+		[1, 0, 1, 0, 1, 0, 1, 0, 1],
+		[0, 1, 0, 1, 0, 1, 0, 1, 0],
+		[1, 0, 1, 0, 1, 0, 1, 0, 1],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 2, 0, 2, 0, 2, 0, 2, 0],
+		[2, 0, 2, 0, 2, 0, 2, 0, 2],
+		[0, 2, 0, 2, 0, 2, 0, 2, 0]
+];
+	var row0 = $("[data-row='0']");
+	var row1 = $("[data-row='1']");
+	var row2 = $("[data-row='2']");
+	var row3 = $("[data-row='3']");
+	var row4 = $("[data-row='4']");
+	var row5 = $("[data-row='5']");
+	var row6 = $("[data-row='6']");
+	var row7 = $("[data-row='7']");
+	
+	$('#game-board').find('.js-row').each(function(i, r) {
+		$(r).find('.js-cell').each(function(j, col) {
+			var $col = $(col);
+			var  $elm;
+			if(gameBoardArray[i][j] == 1){
+				$elm = $redChecker.clone();
+				$col.append($elm);
+				// $col.text('red');
+			}else if(gameBoardArray[i][j] == 2){
+				$elm = $blackChecker.clone();
+				$col.append($elm);
+				// $col.text('black');
+			}
+			
+			if($elm){
+				gameBoardArray[i][j] = $elm;
+			}
+		});
+	})
 
-// lower part
-function initializeRedChecker() {
-  
-}
+	// console.log('---file is loading------');
+	// // alert('this is alert');
+	// /**
+	//  * ============================
+	//  * Game initialization section
+	//  * ============================
+	//  * */ 
+	// function initializeGame() {
+	// 	console.log('-------- game is initializing---------');
+	// 	console.log('-----------row7', row7);
+	// 	// set checker with appropriate data
+	
+	// 	createBlackChecker($cell);
+	// }
 
-function clearCheck(row, column) {
-  
-}
+	// // top part
+	// function initializeBlackChecker() {
+		
+	// }
 
-/** 
- * =================================
- * utility function section
- * =================================
-*/
+	// // lower part
+	// function initializeRedChecker() {
+		
+	// }
 
-function createNewCheck($cell, color) {
-  color = color || 'black-checker'
-}
+	// function clearCheck(row, column) {
+		
+	// }
 
-function createBlackChecker($cell) {
-  var node = document.createElement('<div class="checker black-checker"></div>');
-  $cell.appendChild(node);
-}
+	// /** 
+	//  * =================================
+	//  * utility function section
+	//  * =================================
+	// */
 
-function setDataSet($obj, key, value) {
-  $obj.dataset[key] = value;
-}
+	// function createNewCheck($cell, color) {
+	// 	color = color || 'black-checker'
+	// }
 
-function checker(piece,color,square) {
-	this.id = piece;
-	this.color = color;
-	this.king = false;
-	this.ocupied_square = square;
-	this.alive = true;
-	this.attack = false;
-	if(square%8){
-		this.coordX= square%8;
-		this.coordY = Math.floor(square/8) + 1 ;
-	}
-	else{
-		this.coordX = 8;
-		this.coordY = square/8 ;
-	}
-	this.id.onclick = function  () {
-		showMoves(piece);	
-	}
-}
+	// function createBlackChecker($cell) {
+	// 	var node = document.createElement('<div class="checker black-checker"></div>');
+	// 	$cell.appendChild(node);
+	// }
 
-/**
- * ============== game play section ============
- * */ 
+	// function setDataSet($obj, key, value) {
+	// 	$obj.dataset[key] = value;
+	// }
 
-initializeGame();
+	// function checker(piece,color,square) {
+	// 	this.id = piece;
+	// 	this.color = color;
+	// 	this.king = false;
+	// 	this.ocupied_square = square;
+	// 	this.alive = true;
+	// 	this.attack = false;
+	// 	if(square%8){
+	// 		this.coordX= square%8;
+	// 		this.coordY = Math.floor(square/8) + 1 ;
+	// 	}
+	// 	else{
+	// 		this.coordX = 8;
+	// 		this.coordY = square/8 ;
+	// 	}
+	// 	this.id.onclick = function  () {
+	// 		showMoves(piece);	
+	// 	}
+	// }
+
+	// /**
+	//  * ============== game play section ============
+	//  * */ 
+
+	// initializeGame();
+	// console.log("----------loading js file");
+});
