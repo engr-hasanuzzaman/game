a07577eac981436f5042704f64aefd19a88d0cea
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-07-02 23:39:10 +0600

    Make checker moveable
    -refactor code for making more moduler

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 $(function() {
 	var $gameBoard = $('#game-board');
 	var currentTurn = 'black'; // black or red
+	var $selectedChecker; 
 	var $blackChecker = $('<div class="checker black-checker js-black-checker"></div>');
 	var $redChecker = $('<div class="checker red-checker js-red-checker"></div>');
 	// initial board setting, 1 means red checker, 2 means black checker
@@ -14,7 +15,28 @@ $(function() {
 		[0, 2, -5, 2, -5, 2, -5, 2, 0],
 		[2, -5, 2, -5, 2, -5, 2, -5, 2],
 		[-5, 2, -5, 2, -5, 2, -5, 2, -5]
-];
+	];
+
+	/**==============================================================
+	 * Immediately Invocking function for binding event on each cell
+	 * 1. click on path cell/empty cell
+	 * ============================================================== */
+	(function() {
+		$(document).on('click', '.js-cell', function(e) {
+			console.log('---clicked on cell');
+			var $cell = $(e.target);
+			var rowCol = getCheckerRowColumn($selectedChecker);
+
+			if($cell.hasClass('path')){
+				makeMove($selectedChecker, $cell);
+				gameBoardArray[rowCol[0], rowCol[1]] = 0;
+			}
+
+			resetAllChecker();
+			resetPath();
+		});
+	}());
+
 	var row0 = $("[data-row='0']");
 	var row1 = $("[data-row='1']");
 	var row2 = $("[data-row='2']");
@@ -52,7 +74,7 @@ $(function() {
 
 	/** 
 	 * =================================
-	 * utility function section
+	 * utility functions section
 	 * =================================
 	*/
 
@@ -75,8 +97,11 @@ $(function() {
 		return $elm;
 	}
 
+	// select checker
 	function attachCliceEvent($obj) {
 		$obj.on('click', function(e){
+			e.stopPropagation(); // do not move this event to cell
+			
 			if(isAnySelectedChecker()){
 				resetPath();
 				return resetAllChecker();
@@ -84,13 +109,59 @@ $(function() {
 
 			var $checker = $(e.target);
 			var $cParent = $checker.parent();
+			// check is there any empty path?
+			var paths = getCheckerPath($checker);
+			
+			// if none of the value is cell 
+			if(!paths[0] && !paths[1]){
+				return false;
+			}
+
 			$checker.toggleClass('selected');
-			showCheckerMove($checker);
-			// alert('you have clicked' + $cParent.data('row') + $cParent.data('col'));
+			showCheckerMove(paths);
+			$selectedChecker = $checker; 
 		})
 	}
 	
+	// return array of moveable cells object
+	function getCheckerPath($checker) {
+		var rowColumn = getCheckerRowColumn($checker);
+    var row = rowColumn[0];
+		var col = rowColumn[1];
+		var paths = [];
 
+		if(isBlackChecker($checker)){
+			// black checker path is 7 to 0
+			// first cell will be row - 1, col - 1, row - 1, col + 1
+			var $lCell = $("[data-row='" + (row - 1) + "']" + "[data-col='" + (col - 1) + "']");
+			var $rCell = $("[data-row='" + (row - 1) + "']" + "[data-col='" + (col + 1) + "']");
+			
+			if(gameBoardArray[row - 1][col - 1] == 0){
+				paths[0] = $lCell
+			}
+			
+			if(gameBoardArray[row - 1][col - 1] == 0){
+				paths[1] = $rCell
+			}
+		}else{
+			// red checker path is 0 to 7
+			// first cell will be row + 1, col + 1, row - 1, col + 1
+			var $lCell = $("[data-row='" + (row + 1) + "']" + "[data-col='" + (col - 1) + "']");
+			var $rCell = $("[data-row='" + (row + 1) + "']" + "[data-col='" + (col + 1) + "']");
+			
+			if(gameBoardArray[row + 1][col - 1] == 0){
+				paths[0] = $lCell
+			}
+			
+			if(gameBoardArray[row + 1][col - 1] == 0){
+				paths[1] = $rCell
+			}
+		}
+		
+		return paths;
+	}
+
+	// is there any selected checker?
 	function isAnySelectedChecker() {
 		if($('.checker.selected').length >= 1){
 			return true;
@@ -99,24 +170,14 @@ $(function() {
 		}
 	}
 
-	function showCheckerMove($checker){
-		var rowColumn = getCheckerRowColumn($checker);
-    var row = rowColumn[0];
-		var col = rowColumn[1];
-		if(isBlackChecker($checker)){
-			// black checker path is 7 to 0
-			// first cell will be row - 1, col - 1, row - 1, col + 1
-			var $lCell = $("[data-row='" + (row - 1) + "']" + "[data-col='" + (col - 1) + "']");
-			var $rCell = $("[data-row='" + (row - 1) + "']" + "[data-col='" + (col + 1) + "']");
-			$lCell.addClass('path');
-			$rCell.addClass('path');
-		}else{
-			// red checker path is 0 to 7
-			// first cell will be row + 1, col + 1, row - 1, col + 1
-			var $lCell = $("[data-row='" + (row + 1) + "']" + "[data-col='" + (col - 1) + "']");
-			var $rCell = $("[data-row='" + (row + 1) + "']" + "[data-col='" + (col + 1) + "']");
-			$lCell.addClass('path');
-			$rCell.addClass('path');
+	// set path class to cell 
+	function showCheckerMove(cells){
+		if(cells[0]){
+			cells[0].addClass('path');
+		}
+
+		if(cells[1]){
+			cells[1].addClass('path');
 		}
 	}
 
@@ -151,6 +212,10 @@ $(function() {
 		$('.checker').removeClass('selected');
 	} 
 	
+	// move checker to new cell
+	function makeMove($checker, $newCell) {
+		$checker.detach().appendTo($newCell);
+	}
 	/**
 	 * ============== game play section ============
 	 * */ 
